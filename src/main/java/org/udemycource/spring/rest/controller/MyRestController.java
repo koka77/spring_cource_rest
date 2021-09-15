42ecdeb73859f42c1351f2483eb09e6f0a9336c4
Сомов Александр Сергеевич <ASomov@EAPO.ORG>
2021-09-15 11:44:07 +0300

    Обработка исключений вынесена в отдельный класс при помощь аннотации @ControllerAdvice

--- src/main/java/org/udemycource/spring/rest/controller/MyRestController.java
+++ src/main/java/org/udemycource/spring/rest/controller/MyRestController.java
@@ -1,11 +1,8 @@
 package org.udemycource.spring.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.udemycource.spring.rest.entity.Employee;
-import org.udemycource.spring.rest.exception_handling.EmployeeIncorrectData;
 import org.udemycource.spring.rest.exception_handling.NoSuchEmployeeException;
 import org.udemycource.spring.rest.service.EmployeeService;
 
@@ -36,21 +33,5 @@ public class MyRestController {
         return employee;
     }
 
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleEsception(NoSuchEmployeeException exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-
-    }
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleEsception(Exception exception) {
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-
-    }
 
 }

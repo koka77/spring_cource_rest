3a445c22ee251be85ef87ab7fab023f6f6e21eb1
Сомов Александр Сергеевич <ASomov@EAPO.ORG>
2021-09-15 11:15:49 +0300

    добавил получение работника по id при помощи PathVariable Задача получить ответ по не фиксированному  адресу а по динамическому @GetMapping("/employees/{id}")     public Employee getEmployee(@PathVariable int id) {

--- src/main/java/org/udemycource/spring/rest/controller/MyRestController.java
+++ src/main/java/org/udemycource/spring/rest/controller/MyRestController.java
@@ -2,6 +2,7 @@ package org.udemycource.spring.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.udemycource.spring.rest.entity.Employee;
@@ -22,5 +23,11 @@ public class MyRestController {
         return allEmployees;
     }
 
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        return employee;
+
+    }
 
 }
